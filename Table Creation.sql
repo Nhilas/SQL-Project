fa6f563d6e1d5cff67342adc6879277a4099f3b8
Nhilas <zazlotimircea@gmail.com>
2017-01-06 00:38:30 +0200

    added scripts that populate Item Category and Pricing Application databases

---
+++ Table Creation.sql
@@ -0,0 +1,55 @@
+USE IC; /*IC is Item Catalogue DB*/
+
+declare @x int;
+set @x=1;
+while @x < 6
+	begin
+	insert into IC_CATEGORY values (@x,'Category'+cast(@x as varchar(100)),'Description')
+	set @x=@x+1
+	end
+
+declare @x int;
+set @x=1;
+while @x < 11
+	begin
+	insert into IC_BRAND values (@x,'Brand'+cast(@x as varchar(100)),'Description')
+	set @x=@x+1
+	end
+
+declare @x int;
+set @x=1;
+while @x < 10001
+	begin
+	insert into IC_ITEM values (@x,'Item'+cast(@x as varchar(100)),'Description',rand()*5+1,rand()*10+1)
+	set @x=@x+1
+	end
+
+USE PriA; /*PriA is Pricing Application DB*/
+
+declare @x int;
+declare @y int;
+declare @date date = getdate();
+declare @price int = rand()*1000+1
+declare @up int; /*upper decent-ish margin on price*/
+declare @low int; /*lower decent-ish margin on price*/
+set @x=1;
+set @y=1;
+while @x < 10001 /*item cycle*/
+	begin
+	while @y < 10 /*date cycle*/
+		begin
+		set @up=@price/20 /*basically i don't let price fluctuate for more than +/- 5% on a monthly basis, rolling - i.e. from previous month*/
+		set @low=@price/20
+		if floor(rand()*2)=1 /*check to find out if price drops or rises next month*/
+			set @price = @price+floor(rand()*(@up-@low)+@low)
+			ELSE
+			set @price = @price-floor(rand()*(@up-@low)+@low)
+		insert into PR_PRICE values (@x,@date,@price)
+		set @date = dateadd(month,-1,@date)
+		set @y=@y+1
+		end
+	set @price = rand()*1000+1
+	set @y=1
+	set @date = getdate()
+	set @x=@x+1
+	end
